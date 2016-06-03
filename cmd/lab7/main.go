336b9634db0473eb8b86b2bc5db752d4c30e9e32
Soichi Tanabe <soichit@D-172-28-35-184.dhcp4.washington.edu>
2016-06-02 20:55:46 -0700

    a

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -97,7 +97,7 @@ func main() {
 
 
 	router.GET("getFoodStack", func(c *gin.Context) {
-    rows, err := db.Query("SELECT * FROM food WHERE id = 2")
+    rows, err := db.Query("SELECT * FROM food WHERE id = 3")
         if err != nil {
             c.AbortWithError(http.StatusInternalServerError, err)
             return
