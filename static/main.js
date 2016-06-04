2c171169b8f7c297475e6f7d4b94dbcf179c912d
Soichi Tanabe <soichit@D-172-28-35-41.dhcp4.washington.edu>
2016-06-03 17:17:09 -0700

    a

--- static/main.js
+++ static/main.js
@@ -3,12 +3,13 @@ $(function(){
     var index = 0;
     var jsonData;
     var foodLength;
-    var pastFood = [];
+    var availableFood = [];
 
     window.onload = function() {
         document.getElementById("dislike").onclick = disliked;
         document.getElementById("like").onclick = liked;
         getData();
+        availableFood();
     };
 
     $.get("/ping", function(data){
@@ -31,14 +32,23 @@ $(function(){
             document.getElementById("foodImage").src = jsonUrl;
         }, "json")
     }
+
+    function availableFood() {
+        for (int i = 0; i < foodLength.length; i++) {
+            availableFood.push(i);
+        }
+        console.log(availableFood);
+    }
     
 
     function disliked() {
-        index = Math.floor(Math.random() * (foodLength - 1)); //random # between 0 and foodLenth - 1
-        console.log(index);
-
         document.getElementById("youMatched").innerHTML = "";
-        if (index >= foodLength - 1) { //size - 1
+        removeIndex = Math.floor(Math.random() * availableFood.length); //random # between 0 and availableFood.length
+        index = availableFood[removeIndex];
+        console.log(index);
+        availableFood.splice(removeIndex, 1);
+        
+        if (availableFood.length == 0) { //size - 1
             document.getElementById("foodName").innerHTML = "----";
             document.getElementById("foodPrice").innerHTML = "";
             document.getElementById("foodImage").src = 'static/img/empty.jpg';
@@ -48,7 +58,7 @@ $(function(){
     }
 
     function liked() {
-        if (index < foodLength - 1) {
+        if (availableFood.length > 0) {
             getData();
             document.getElementById("youMatched").innerHTML = "It's a Match! ...with " + jsonName;
         }
